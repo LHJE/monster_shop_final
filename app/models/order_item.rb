3de0dc473f743e5d6a89f904d66d075dda5544f0
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-20 18:14:08 -0600

    Update subtotal method to adhere to discounts.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,7 +3,24 @@ class OrderItem < ApplicationRecord
   belongs_to :item
 
   def subtotal
-    quantity * price
+    @order_items = OrderItem.where(item_id: item.id)
+    reverse_discounts = Discount.order(:percent)
+    @discounts = reverse_discounts.reverse
+    if @discounts != [] && @order_items != []
+      total = []
+      @order_items.each do |order_item|
+        @discounts.each do |discount|
+          if item.merchant_id == discount.merchant_id && discount.active && order.id == order_item.order_id && order_item.quantity >= discount.min_items
+            total = (quantity * price) - ((quantity * price) * (discount.percent / 100.0))
+            break
+          else
+            total = quantity * price
+          end
+        end
+      end
+      total
+    end
+
   end
 
   def fulfill
