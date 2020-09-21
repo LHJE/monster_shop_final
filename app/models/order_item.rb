d79e0303d4b5f54f7b50e7d5041b8bac7ce08dd8
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-21 13:41:12 -0600

    Correct logic in order_item model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,20 +6,28 @@ class OrderItem < ApplicationRecord
     @order_items = OrderItem.where(item_id: item.id)
     reverse_discounts = Discount.order(:percent)
     @discounts = reverse_discounts.reverse
+
     if @discounts != [] && @order_items != []
-      total = []
+      if @total.nil?
+        @total = 0
+      else
+        @total = @total
+      end
       @order_items.each do |order_item|
         @discounts.each do |discount|
           if item.merchant_id == discount.merchant_id && discount.active && order.id == order_item.order_id && order_item.quantity >= discount.min_items
-            total = (quantity * price) - ((quantity * price) * (discount.percent / 100.0))
-            break
-          else
-            total = quantity * price
+            @total += (quantity * price) - ((quantity * price) * (discount.percent / 100.0))
           end
         end
+        if @total == 0
+          @total += quantity * price
+        end
       end
-      total
+    else
+
+      @total = quantity * price
     end
+    @total
 
   end
 
