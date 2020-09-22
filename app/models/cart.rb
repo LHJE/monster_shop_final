300d9f19787fe699aa497648c25ad0635d7dfe96
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-22 16:40:41 -0600

    Refactor Cart grand total method using AR

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -30,20 +30,12 @@ class Cart
     @contents.each do |item_id, quantity|
       items[Item.find(item_id)]= quantity
     end
-    reverse_discounts = Discount.order(:percent)
-    @discounts = reverse_discounts.reverse
     items.each do |item, quantity|
+      @discounts = Discount.where(active: true).where(merchant_id: item.merchant_id).where("min_items <= #{quantity}").order(percent: :desc).pluck(:percent)
       if @discounts == []
         grand_total += item.price * quantity
       else
-        @discounts.each do |discount|
-          if discount.merchant_id == item.merchant.id && discount.active && quantity >= discount.min_items
-            grand_total += (quantity * item.price) - ((quantity * item.price) * (discount.percent / 100.0))
-          else
-            grand_total += item.price * quantity
-            break
-          end
-        end
+        grand_total += (quantity * item.price) - ((quantity * item.price) * (@discounts[0] / 100.0))
       end
     end
     grand_total
@@ -55,6 +47,10 @@ class Cart
 
   def subtotal_of(item_id)
     @contents[item_id.to_s] * Item.find(item_id).price
+  end
+
+  def subtotal_with_discount(cart, item_id, discount)
+    cart.subtotal_of(item_id) - (cart.subtotal_of(item_id) * (discount.percent / 100.0))
   end
 
   def limit_reached?(item_id)
