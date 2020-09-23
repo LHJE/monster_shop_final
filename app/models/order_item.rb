b2da2bea1934af1d900a156b0e1eb05118e78b94
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-09-22 18:06:18 -0600

    Refactor Order Items model with AR

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,30 +3,21 @@ class OrderItem < ApplicationRecord
   belongs_to :item
 
   def subtotal
-    @order_items = OrderItem.where(item_id: item.id)
-    reverse_discounts = Discount.order(:percent)
-    @discounts = reverse_discounts.reverse
-    if @discounts != [] && @order_items != []
-      if @total.nil?
-        @total = 0
-      else
-        @total = @total
-      end
-      @order_items.each do |order_item|
-        @discounts.each do |discount|
-          if item.merchant_id == discount.merchant_id && discount.active && order.id == order_item.order_id && order_item.quantity >= discount.min_items
-            @total += (quantity * price) - ((quantity * price) * (discount.percent / 100.0))
-          end
-        end
-        if @total == 0
-          @total += quantity * price
-        end
-      end
-    else
-      @total = quantity * price
+    if @total.nil?
+      @total = 0
     end
-    @total
-
+    @order_item = OrderItem.where(item_id: item.id).where(order_id: order.id).reduce
+    if @order_item == []
+      @total = quantity * price
+    else
+      @discounts = Discount.where(active: true).where(merchant_id: item.merchant_id).where("min_items <= #{@order_item.quantity}").order(percent: :desc).pluck(:percent)
+      if @discounts == []
+        @total += quantity * price
+      else
+        @total += (quantity * price) - ((quantity * price) * (@discounts[0] / 100.0))
+      end
+    end
+    @total.round(2)
   end
 
   def fulfill
